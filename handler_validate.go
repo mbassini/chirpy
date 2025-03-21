95213f85740237be935ff36581c1644db4cd7945
Mauro <mbassini@outlook.com>
2025-03-21 17:02:37 -0300

    Sanitize chirp

--- handler_validate.go
+++ handler_validate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +11,7 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		Body string `json:"body"`
 	}
 	type validResponse struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	var req chirpRequest
@@ -25,5 +26,25 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, validResponse{Valid: true})
+	cleanChirp(&req.Body)
+
+	respondWithJSON(w, http.StatusOK, validResponse{CleanedBody: req.Body})
+}
+
+func cleanChirp(s *string) {
+	bannedWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	words := strings.Split(*s, " ")
+	for i, word := range words {
+		_, isBanned := bannedWords[strings.ToLower(word)]
+		if isBanned {
+			words[i] = "****"
+		}
+	}
+
+	*s = strings.Join(words, " ")
 }
